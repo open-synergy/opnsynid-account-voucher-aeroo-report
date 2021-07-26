76a2da2378f5e79e8b7c17e7ef35546fcf02f18c
Nurazmi <azmimr67@gmail.com>
2021-07-26 19:31:25 +0700

    [8.0.1.0.0] account_voucher_bank_cash_aeroo_report
    Fix #1

---
+++ account_voucher_bank_cash_aeroo_report/reports/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 201521penSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import (
+    parser_account_voucher_bank,
+    parser_account_voucher_cash,
+)
