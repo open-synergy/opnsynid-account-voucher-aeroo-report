d82e7524eb199342438b6a9c39ad987350d6b026
Nurazmi <azmimr67@gmail.com>
2021-08-08 12:47:33 +0700

    [8.0.1.0.0] account_voucher_invoice_settlement_aeroo_report
    Fix #6

---
+++ account_voucher_invoice_settlement_aeroo_report/__openerp__.py
@@ -0,0 +1,27 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author/
+{
+    "name": "Invoice Settlement Voucher Aeroo Report",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "depends": [
+        "account_voucher_invoice_settlement",
+        "report_aeroo",
+        "base_amount_to_text",
+        "base_print_policy",
+    ],
+    "data": [
+        "reports/report_account_voucher_invoice_settlement.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "license": "AGPL-3",
+}
